138a9b06a2b1fd442269483896b96c383f9c4bab
Manis Thapa <thapa.manish27@gmail.com>
2024-08-17 14:04:41 +0530

    Switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Products from './components/product/products';
 // import About from './components/About';
 // import Contact from './components/Contact';
